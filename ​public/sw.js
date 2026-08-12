e844fc3626485aa7792516bb83acf821a1a59867
Hamro-kirana-pasal <gckamal678@gmail.com>
2026-08-12 21:09:25 +0530

    Update sw.js

--- ​public/sw.js
+++ ​public/sw.js
@@ -1,15 +1,38 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('mychat-cache-v2').then((cache) => {
-      return cache.addAll(['/', '/index.html']);
+const CACHE_NAME = 'mychat-pwa-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
