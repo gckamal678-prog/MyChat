fce4ef7098ea548574485dfa64671e80111bf74e
Hamro-kirana-pasal <gckamal678@gmail.com>
2026-08-12 21:55:36 +0530

    Update ​vite.config.js

--- ​vite.config.js
+++ ​vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
+  build: {
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // ठूला लाइब्रेरीहरू अलग गर्छ
+          }
+        }
+      }
+    }
   }
-});
+})
