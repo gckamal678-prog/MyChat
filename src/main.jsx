66b5861f1f2fc79981d83ceed51870e070327b88
Hamro-kirana-pasal <gckamal678@gmail.com>
2026-08-12 21:10:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { registerServiceWorker } from './utils/registerSW'
 
+// Register PWA Service Worker
+registerServiceWorker();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
